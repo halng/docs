8deffd9689c5e696c9433e84d138e177ef99edd2
harold <haonguyentan2001@gmail.com>
2023-12-03 16:49:27 +0700

    test

--- script/pre-merged.py
+++ script/pre-merged.py
@@ -17,35 +17,34 @@ if __name__ == "__main__":
     )
 
     g = GitUtils(remote_branch="dev", current_branch=branch)
-    if g.is_run():
-        if len(g.get_category_change()) > 0:
-            msg = "> CATEGORY "
-            for x in g.get_category_change():
-                if str(x["_path"]).endswith("yaml"):
-                    msg = msg + \
-                        f'\n- {get_action(None, x["_type"]).name} for category `{str(x["_path"]).split("/")[1]}` under `{str(x["_path"]).split("/")[0]}`'
-            alert_slack(msg)
+    if len(g.get_category_change()) > 0:
+        msg = "> CATEGORY "
+        for x in g.get_category_change():
+            if str(x["_path"]).endswith("yaml"):
+                msg = msg + \
+                    f'\n- {get_action(None, x["_type"]).name} for category `{str(x["_path"]).split("/")[1]}` under `{str(x["_path"]).split("/")[0]}`'
+        alert_slack(msg)
         g.comment_pr(msg)
-        if len(g.get_blog_change()) > 0:
-            print(g.get_blog_change())
-            msg = "> BLOG "
-            for x in g.get_blog_change():
-                action = ""
-                if x["_path"].endswith("yaml"):
-                    action = (
-                        Action.UPDATE_METADATA
-                        if x["_type"] == "M"
-                        else Action.CREATE_NEW
-                    )
-                elif x["_path"].endswith("md"):
-                    action = (
-                        Action.UPDATE_CONTENT
-                        if x["_type"] == "M"
-                        else Action.CREATE_NEW
-                    )
-                msg = (
-                    msg
-                    + f"\n- {action.name} for blog id: `{str(x['_path']).split('/')[2]}` under `{x['_path'].split('/')[0]} -> {x['_path'].split('/')[1]}`"
+    if len(g.get_blog_change()) > 0:
+        print(g.get_blog_change())
+        msg = "> BLOG "
+        for x in g.get_blog_change():
+            action = ""
+            if x["_path"].endswith("yaml"):
+                action = (
+                    Action.UPDATE_METADATA
+                    if x["_type"] == "M"
+                    else Action.CREATE_NEW
                 )
-            alert_slack(msg)
-            g.comment_pr(msg)
+            elif x["_path"].endswith("md"):
+                action = (
+                    Action.UPDATE_CONTENT
+                    if x["_type"] == "M"
+                    else Action.CREATE_NEW
+                )
+            msg = (
+                msg
+                + f"\n- {action.name} for blog id: `{str(x['_path']).split('/')[2]}` under `{x['_path'].split('/')[0]} -> {x['_path'].split('/')[1]}`"
+            )
+        alert_slack(msg)
+        g.comment_pr(msg)
