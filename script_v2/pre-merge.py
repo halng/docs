7642f744fa3b56ab71d12423513cfa039613a5ec
Bot <haonguyentan2001@gmail.com>
2023-12-09 17:54:35 +0700

    update

--- script_v2/pre-merge.py
+++ script_v2/pre-merge.py
@@ -4,11 +4,11 @@ from utils import get_all_changes, alert_slack
 branch = os.getenv("CURRENT_BRANCH", "dev-test")
 pr_number = os.getenv("PR_NUMBER", "1")
 
-# alert_slack(
-#         f"Hi <!here>. Have some change in\n- pr https://github.com/tanhaok/docs/pull/{pr_number} \n- Branch: https://github.com/tanhaok/docs/tree/{branch}"
-#     )
+alert_slack(
+        f"Hi <!here>. Have some change in\n- pr https://github.com/tanhaok/docs/pull/{pr_number} \n- Branch: https://github.com/tanhaok/docs/tree/{branch}"
+    )
 
-all_changes = get_all_changes("dev-004", "dev")
+all_changes = get_all_changes(branch, "dev")
 
 for x in all_changes:
     action_type, _path = x.split('_')
@@ -17,8 +17,8 @@ for x in all_changes:
     try:
         int(_path.split("/")[-2])
         obj = "BLOG_META" if str(_path).endswith("yaml") else "BLOG_CONTENT"
-        print(f'`{action}` the `{obj}` in path: `{_path.replace("/", " > ")}`')
+        alert_slack(f'`{action}` the `{obj}` in path: `{_path.replace("/", " > ")}`')
     except ValueError:
         if str(_path).endswith("yaml"):
-            print(f'`{action}` the `CATEGORY` in path: `{_path.replace("/", " > ")}`')
+            alert_slack(f'`{action}` the `CATEGORY` in path: `{_path.replace("/", " > ")}`')
     
