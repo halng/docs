8a3dd44150029d908e36c72c020c160b894b456c
Bot <haonguyentan2001@gmail.com>
2023-12-09 18:04:31 +0700

    update file merged.py to prepare next work

---
+++ script_v2/merged.py
@@ -0,0 +1,6 @@
+"""
+1. TODO: Send request to server to create/update.
+2. TODO: Update file `info.yaml` after receive response.
+3. TODO: Update the field cateId in blog metadata in case create new category.
+4. FIXME: if can, update content in README.md file after create new blog success.
+"""
